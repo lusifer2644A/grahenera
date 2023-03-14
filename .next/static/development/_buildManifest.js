d8bf110b02cec949aa5caf6cd29a5870eebec853
ankit-gog <ankit.k@goglocal.com>
2023-03-15 00:05:09 +0530

    Added product info in warrenty check 2

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/check-warrenty":["static\u002Fchunks\u002Fpages\u002Fcheck-warrenty.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcheck-warrenty"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
